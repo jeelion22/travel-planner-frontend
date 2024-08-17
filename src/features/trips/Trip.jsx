441d14c103363e614dc6e3255d98297860325502
jeeva m <jeelion22@gmail.com>
2024-08-18 01:07:07 +0530

    detailed budget tracker added

--- src/features/trips/Trip.jsx
+++ src/features/trips/Trip.jsx
@@ -21,10 +21,56 @@ const Trip = () => {
   const trip = useSelector(selectTrip);
   const error = useSelector(selectTripError);
 
+  const calculateBudget = (arr) => {
+    const bud = arr.filter((item) =>
+      [
+        "transportationBudget",
+        "accommodationBudget",
+        "foodBudget",
+        "otherBudget",
+      ].includes(item[0])
+    );
+
+    return bud.reduce((total, [key, value]) => {
+      if (typeof value === "number") {
+        return total + value;
+      }
+      return total;
+    }, 0);
+  };
+
+  const calculateSpent = (arr) => {
+    const bud = arr.filter((item) =>
+      ["transportation", "accommodation", "food", "other"].includes(item[0])
+    );
+
+    return bud.reduce((total, [key, value]) => {
+      if (typeof value === "number") {
+        return total + value;
+      }
+      return total;
+    }, 0);
+  };
+
+  const totalBudgetOverRun = (budgetArr, spentArr) => {
+    if (calculateBudget(budgetArr) - calculateSpent(spentArr) < 0) {
+      return calculateSpent(spentArr) - calculateBudget(budgetArr);
+    }
+    return 0;
+  };
+
+  const calculatebudgetOverRun = (budgetAmt, spentAmt) => {
+    if (budgetAmt - spentAmt < 0) {
+      return spentAmt - budgetAmt;
+    }
+    return 0;
+  };
+
   useEffect(() => {
     if (tripId) {
       dispatch(getTripById(tripId))
         .unwrap()
+        .then((res) => console.log(res))
         .catch((err) => {
           if (error) {
             alert(error);
@@ -52,17 +98,16 @@ const Trip = () => {
     );
   }
 
-  // if (status === "failed") {
-  //   return <div>Error: {error || "Trip not found"}</div>;
-  // }
+  if (status === "failed") {
+    return <div>Error: {error || "Trip not found"}</div>;
+  }
 
-  return (
-    <div className="container">
-      <div className="border rounded m-4 p-4 bg-body-tertiary">
-        <div class="row row-cols-1 row-cols-md-3 g-4 text-center">
-          <div class="col">
+  if (status === "succeeded") {
+    return (
+      <div className="container">
+        <div className="row bg-body-tertiary mt-4 rounded p-4 text-center">
+          <div class="col-md-4 col-sm-6 mb-3 mb-sm-0">
             <div class="card h-100">
-              {/* <img src="..." class="card-img-top" alt="..."> */}
               <div class="card-body">
                 <h5 class="card-title">{trip.tripName}</h5>
                 <p class="card-text">{trip.destination}</p>
@@ -87,17 +132,116 @@ const Trip = () => {
               </div>
             </div>
           </div>
-          <div class="col">
+          <div class="col-md-8 col-sm-6 ">
             <div class="card h-100">
-              {/* <img src="..." class="card-img-top" alt="..."> */}
               <div class="card-body">
                 <h5 class="card-title">Budget</h5>
-                <p class="card-text">
-                  <small class="text-body-secondary">
-                    {getSymbolFromCurrency(`${trip.budget.currency}`)}{" "}
-                    {trip.budget.amount}
-                  </small>
-                </p>
+
+                <table class="table table-striped text-start">
+                  <thead>
+                    <tr>
+                      <th scope="col">Category</th>
+                      <th scope="col">Original Budget</th>
+                      <th scope="col">Actual Spending</th>
+                      <th scope="col">Budget Overrun</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Trans.</td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {trip.budget.transportationBudget}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {trip.budget.transportation}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {calculatebudgetOverRun(
+                          trip.budget.transportationBudget,
+                          trip.budget.transportation
+                        )}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Accom.</td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {trip.budget.accommodationBudget}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {trip.budget.accommodation}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {calculatebudgetOverRun(
+                          trip.budget.accommodationBudget,
+                          trip.budget.accommodation
+                        )}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Food</td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {trip.budget.foodBudget}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {trip.budget.food}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {calculatebudgetOverRun(
+                          trip.budget.foodBudget,
+                          trip.budget.food
+                        )}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Other</td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {trip.budget.otherBudget}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {trip.budget.other}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {calculatebudgetOverRun(
+                          trip.budget.otherBudget,
+                          trip.budget.other
+                        )}
+                      </td>
+                    </tr>
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <td>Total</td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {calculateBudget(Object.entries(trip.budget))}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(trip.budget.currency)}
+                        {calculateSpent(Object.entries(trip.budget))}
+                      </td>
+                      <td>
+                        {getSymbolFromCurrency(`${trip.budget.currency}`)}
+                        {totalBudgetOverRun(
+                          Object.entries(trip.budget),
+                          Object.entries(trip.budget)
+                        )}
+                      </td>
+                      <td></td>
+                    </tr>
+                  </tfoot>
+                </table>
               </div>
               <div class="card-footer">
                 <small class="text-body-secondary">
@@ -107,9 +251,56 @@ const Trip = () => {
             </div>
           </div>
         </div>
+        <div className="row bg-body-tertiary mt-4 rounded p-4">
+          <div class="col -sm-6 mb-3 mb-sm-0 h-100">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Budget</h5>
+                <p class="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row bg-body-tertiary mt-4 rounded p-4">
+          <div class="col-sm-6 mb-3 mb-sm-0 h-100">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 h-100">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Trip;
