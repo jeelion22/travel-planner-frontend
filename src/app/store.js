6527238cd598e5764963a29473fc44f7a6a205f9
jeeva m <jeelion22@gmail.com>
2024-08-18 22:37:01 +0530

    added toDoSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/users/usersSlice";
 import tripReducer from "../features/trips/tripSlice";
+import toDoReducer from "../features/toDos/toDoSlice";
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
     trips: tripReducer,
+    toDos: toDoReducer,
   },
 });
