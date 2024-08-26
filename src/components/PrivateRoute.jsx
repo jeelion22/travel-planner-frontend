a84fa2c3f953f1fe05297ecd86bf8bc57b7b2b1e
jeeva m <jeelion22@gmail.com>
2024-08-27 01:17:43 +0530

    loader icon added

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import {
@@ -12,13 +14,24 @@ const PrivateRoute = ({ children }) => {
   const status = useSelector(selectUserStatus);
   const dispatch = useDispatch();
 
-
   useEffect(() => {
     dispatch(getUser());
   }, [dispatch]);
 
   if (status === "loading") {
-    return <div>Loading...</div>;
+    return (
+      <div className="container">
+        <div
+          className="row justify-content-center align-items-center"
+          style={{ height: "50vh" }}
+        >
+          <div className="col text-center">
+            <FontAwesomeIcon icon={faSpinner} spinPulse size="4x" />
+            <h6 className="mt-4">Loading...</h6>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   if (status === "failed") {
