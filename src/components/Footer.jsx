795284a4880593751f5453b95852ca4e7101547d
jeeva m <jeelion22@gmail.com>
2024-08-27 01:13:50 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="bg-body-tertiary text-center py-3 fixed-bottom">
+      <div className="container">
+        <p className="mb-0">&copy; {year} Travel-Planner. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
